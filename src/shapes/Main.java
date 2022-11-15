efa8ff1ef36e1bb8d8a8b1857ece9712292d64a3
Hassan2K23 <113380661+Hassan2K23@users.noreply.github.com>
2022-11-15 14:00:34 -0500

    Made Some Progress
    Worked on Main (Was able to make the layout and only make number 4 function)

--- src/shapes/Main.java
+++ src/shapes/Main.java
@@ -1,10 +1,34 @@
 package shapes;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		int userChoice;
+		do {
+			System.out.println("Do you want to:");
+			System.out.println("1. Create a shape");
+			System.out.println("2. View a shape");
+			System.out.println("3. Change a shape");
+			System.out.println("4. Quit");
+			try {
+				userChoice = in.nextInt();
+			} catch (InputMismatchException e) {
+				userChoice = 4;
+				System.out.println("I guess you don't want to use this program today");
+			}
+			
+			if (userChoice==1) {
+				
+			}else if (userChoice==2) {
+				
+			}else if (userChoice==3) {
+				
+			}
+		} while (userChoice != 4);
 	}
-
 }
